9ce2d397883c116bd912229caa12dbd8ee62a7e9
bhendee <85202956+bhendee@users.noreply.github.com>
2023-04-11 10:41:41 -0700

    get ridda that stinky nonsnese

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def benjamin(kevin_steinacker):
     print(f'benjamin {kevin_steinacker}')
-    print('OH BROTHER THIS GUY STINKS!!')
+    # print('OH BROTHER THIS GUY STINKS!!')
 
 if __name__ == '__main__':
     benjamin('kevin steinacker')
