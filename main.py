eae5f8ebc84b3c33d6740428718b2ec3f8030b66
bhendee <85202956+bhendee@users.noreply.github.com>
2023-04-11 11:26:35 -0700

    we do a bit of slicing

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ if __name__ == '__main__':
     x = (3 + 2 - 10) / 15
     var = 'hello'
     print((3**2+4**2)**0.5)
+    fun_list = list(range(1,10))
+    print(fun_list[4])
+    print(fun_list[4:6])
+    fun_list[-1] = 10
+    fun_string = '123456789'
+    print(fun_string[4])
+    print(fun_string[4:6])
+    fun_string = fun_string[:-1] + '10'
