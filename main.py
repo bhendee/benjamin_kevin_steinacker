cc9841e70538f3055e686502cf937d5646846dcd
bhendee <85202956+bhendee@users.noreply.github.com>
2023-04-11 11:08:18 -0700

    do some printin

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def benjamin(kevin_steinacker):
     # print('OH BROTHER THIS GUY STINKS!!')
 
 if __name__ == '__main__':
-    benjamin('kevin steinacker')
-    benjamin('hendee smith')
+    x = (3 + 2 - 10) / 15
+    var = 'hello'
+    print((3**2+4**2)**0.5)
